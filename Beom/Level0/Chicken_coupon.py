b61fb419f569d30c76ca4d6953e213286de8b6a3
Beom <113332125+BeomKung@users.noreply.github.com>
2023-05-31 18:45:42 +0900

    Update Chicken_coupon.py

--- Beom/Level0/Chicken_coupon.py
+++ Beom/Level0/Chicken_coupon.py
@@ -1,1 +1,14 @@
+def solution(chicken): 
+    coupon = chicken 
+    answer = 0 
+    while coupon >= 10:
+        service = coupon//10
+        answer += service
+        coupon = coupon % 10 + service     
+
+    return answer
+
+
+print(solution(100))
+print(solution(1081))
 
