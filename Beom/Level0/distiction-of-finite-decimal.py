61eb2cbf7e5dfa62eaa66af982c77519d3d0180b
Beom <113332125+BeomKung@users.noreply.github.com>
2023-06-04 08:49:04 +0900

    Create distiction-of-finite-decimal.py

---
+++ Beom/Level0/distiction-of-finite-decimal.py
@@ -0,0 +1,22 @@
+def GCD(a,b): #greatest common denominator(최대공약수)
+    while b != 0:
+        a, b = b, a % b
+    return a
+def solution(a, b):
+    gcd = GCD(a, b)
+    a //= gcd
+    b //= gcd
+
+    while b % 2 == 0:
+        b //= 2
+    while b % 5 == 0:
+        b //= 5
+    
+    if b == 1:
+        return 1
+    else: 
+        return 2
+
+print(solution(7,20))
+print(solution(11,22))
+print(solution(12,21))
