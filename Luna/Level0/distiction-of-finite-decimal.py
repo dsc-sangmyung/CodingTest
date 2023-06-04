76d1db5ff0298c6c785ebd6c2725acfa86d9fbff
KimYeonAh <50138845+KimYeonAh@users.noreply.github.com>
2023-06-04 22:38:19 +0900

    Update distiction-of-finite-decimal.py

--- Luna/Level0/distiction-of-finite-decimal.py
+++ Luna/Level0/distiction-of-finite-decimal.py
@@ -3,11 +3,11 @@
 import math
 
 def solution(a, b):
-    tmp = b//math.gcd(a,b)
+    tmp = b // math.gcd(a,b)
 
-    while tmp%2 == 0 :
+    while tmp % 2 == 0 :
         tmp //= 2
-    while tmp%5 == 0 :
+    while tmp % 5 == 0 :
         tmp //= 5
     
     return 1 if tmp == 1 else 2
