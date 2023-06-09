940328476d2523b6432ff012b2e974620eab916a
KimYeonAh <50138845+KimYeonAh@users.noreply.github.com>
2023-06-10 04:37:54 +0900

    Create the-number-of-curse.py

---
+++ Luna/Level0/the-number-of-curse.py
@@ -0,0 +1,11 @@
+# 저주의 숫자 3
+
+def solution(n):
+    answer = 0
+    
+    for _ in range(n) :
+        answer += 1  
+        while answer % 3 == 0 or '3' in str(answer) :
+            answer += 1
+
+    return answer
