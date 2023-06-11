b388f24b6b1fbf5aca9d1c8887559190d17eb58b
SangJun <73101224+goodpinokio@users.noreply.github.com>
2023-06-11 15:10:18 +0900

    저주의 3

---
+++ Maverick/Level0/the-number-of-curse.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    for i in range(n):
+        answer +=1
+        while answer % 3 ==0 or '3' in str(answer):
+            answer+=1
+    
+    return answer
+
+a = int(input())
+print(solution(a))
