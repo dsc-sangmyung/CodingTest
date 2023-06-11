246e7435df6ec5d4083741a7504b5c0d8c93e2df
Beom <113332125+BeomKung@users.noreply.github.com>
2023-06-11 21:03:05 +0900

    Create the-number-of-curse.py

---
+++ Beom/Level0/the-number-of-curse.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    for i in range(n):
+        answer += 1
+        while answer%3 == 0 or '3' in str(answer):
+            answer += 1
+    return answer
+
+print(solution(15))
+print(solution(40))
